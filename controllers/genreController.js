f8c32a76866626c79ff9584387c87aaa41c8f305
bilgramiraza <bilgramiraza@yahoo.com>
2022-11-01 11:37:55 +0530

    Setup boilerplate code for Genre Controller

---
+++ controllers/genreController.js
@@ -0,0 +1,33 @@
+const Genre = require('../models/genre');
+
+exports.genre_list = (req, res) => {
+  res.send('NOT IMPLEMENTED: Genre List');
+};
+
+exports.genre_detail = (req, res) => {
+  res.send(`NOT IMPLEMENTED: Genre Details ${req.params.id}`);
+};
+
+exports.genre_create_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: Genre Create GET');
+};
+
+exports.genre_create_post = (req, res) => {
+  res.send('NOT IMPLEMENTED: Genre Create POST');
+};
+
+exports.genre_delete_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: Genre Delete GET');
+};
+
+exports.genre_delete_post = (req, res) => {
+  res.send('NOT IMPLEMENTED: Genre Delete POST');
+};
+
+exports.genre_update_get = (req, res) => {
+  res.send('NOT IMPLEMENTED: Genre Update GET');
+};
+
+exports.genre_update_post = (req, res) => {
+  res.send('NOT IMPLEMENTED: Genre Update POST');
+};
