a1835911ea78d7cdcd3c992aa5e84755cae20255
bilgramiraza <bilgramiraza@yahoo.com>
2022-11-13 11:32:21 +0530

    Setup Post Route for Author Creation Form

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -39,9 +39,47 @@ exports.author_detail = (req, res, next) => {
   );
 };
 
-exports.author_create_get = (req, res, next) => {
-  res.render('author_form', { title: 'Author Creation Form' });
-};
+exports.author_create_get = [
+  body('first_name')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('First Name Must Be Specified'),
+  body('family_name')
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .withMessage('Family Name Must Be Specified'),
+  body('date_of_birth', 'Invalid Date of Birth')
+    .optional({ checkFalsy: 'true' })
+    .isISO8601()
+    .toDate(),
+  body('date_of_death', 'Invalid Date of Death')
+    .optional({ checkFalsy: 'true' })
+    .isISO8601()
+    .toDate(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.render('author_form', {
+        title: 'Create Author',
+        author: req.body,
+        errors: errors.array(),
+      });
+      return;
+    }
+    const author = new Author({
+      first_name: req.body.first_name,
+      family_name: req.body.family_name,
+      date_of_birth: req.body.date_of_birth,
+      date_of_death: req.body.date_of_death,
+    });
+    author.save((err) => {
+      if (err) return next(err);
+      res.redirect(author.url);
+    });
+  },
+];
 
 exports.author_create_post = (req, res) => {
   res.send('NOT IMPLEMENTED: Author Create POST ');
