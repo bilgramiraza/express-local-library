1e0eeeef0c2f475f1becac0c55449c51989613b1
bilgramiraza <bilgramiraza@yahoo.com>
2022-11-23 08:31:06 +0530

    Added Post Route for Deleteing Genre

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -91,8 +91,32 @@ exports.genre_delete_get = (req, res, next) => {
   );
 };
 
-exports.genre_delete_post = (req, res) => {
-  res.send('NOT IMPLEMENTED: Genre Delete POST');
+exports.genre_delete_post = (req, res, next) => {
+  async.parallel(
+    {
+      genre(callback) {
+        Genre.findById(req.body.genreid).exec(callback);
+      },
+      genre_books(callback) {
+        Book.find({ genre: req.body.genreid }).exec(callback);
+      },
+    },
+    (err, results) => {
+      if (err) return next(err);
+      if (results.genre_books.length > 0) {
+        res.render('genre_delete', {
+          title: 'Delete Genre',
+          genre: results.genre,
+          genre_books: results.genre_books,
+        });
+        return;
+      }
+      Genre.findByIdAndDelete(req.body.genreid, (err) => {
+        if (err) return next(err);
+        res.redirect('/catalog/genres');
+      });
+    }
+  );
 };
 
 exports.genre_update_get = (req, res) => {
